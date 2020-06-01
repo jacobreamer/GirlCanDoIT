dbd599a9c8e1b8b423167002b90a1f2e12504b46
Safa Alkhamis <Safa Alkhamis>
2020-06-01 15:36:35 -0400

    Project

---
+++ GirlsCanDoIT/GirlsCanDoIT/ImageResourceExtention.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GirlsCanDoIT
+{
+    [ContentProperty(nameof(Source))]
+    internal class ImageResourceExtention : IMarkupExtension
+    {
+        public string Source { get; set; }
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+            {
+                return null;
+            }
+
+            var imageSource = ImageSource.FromResource(Source, typeof(ImageResourceExtention).GetTypeInfo().Assembly);
+
+            return imageSource;
+        }
+    }
+}
---
+++ GirlsCanDoIT/GirlsCanDoIT/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GirlsCanDoIT
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BooksButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new AcademicsPage());
+        }
+
+        private void laptopButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new CareersPage());
+        }
+
+        private void womenButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new WomenInITPage());
+        }
+
+        private void infoButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ProfessionalResourcesPage());
+        }
+    }
+}
