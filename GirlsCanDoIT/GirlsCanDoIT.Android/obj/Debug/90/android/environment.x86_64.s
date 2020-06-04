13e47db2a0205a6bfcaf96456d6d9a618853b9c3
jacobreamer <reamerj3@ferris.edu>
2020-06-03 20:44:29 -0400

    Updated Pro Resources Page

--- GirlsCanDoIT/GirlsCanDoIT.Android/obj/Debug/90/android/environment.x86_64.s
+++ GirlsCanDoIT/GirlsCanDoIT.Android/obj/Debug/90/android/environment.x86_64.s
@@ -72,7 +72,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"b118f81f-023e-488e-a68d-1b401ba4a47b"
+	.asciz	"1ea4ece4-06ce-4262-8dbe-b73f013c80e1"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
--- GirlsCanDoIT/GirlsCanDoIT/obj/Debug/netstandard2.0/ProfessionalResourcesPage.xaml.g.cs
+++ GirlsCanDoIT/GirlsCanDoIT/obj/Debug/netstandard2.0/ProfessionalResourcesPage.xaml.g.cs
@@ -17,11 +17,15 @@ namespace GirlsCanDoIT {
     public partial class ProfessionalResourcesPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.ScrollView MainScroll;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private global::Xamarin.Forms.Picker picker;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(ProfessionalResourcesPage));
+            MainScroll = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.ScrollView>(this, "MainScroll");
             picker = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Picker>(this, "picker");
         }
     }
