e4fa15498d3efc55b1305cd41e00138056af23c2
jacobreamer <reamerj3@ferris.edu>
2020-06-04 18:06:11 -0400

    More edits to Women in IT and Pro Resources

--- GirlsCanDoIT/GirlsCanDoIT/ProfessionalResourcesPage.xaml.cs
+++ GirlsCanDoIT/GirlsCanDoIT/ProfessionalResourcesPage.xaml.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -11,10 +12,36 @@ namespace GirlsCanDoIT
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ProfessionalResourcesPage : ContentPage
-    {
+    { // Launcher.OpenAsync is provided by Xamarin.Essentials.
+        
         public ProfessionalResourcesPage()
         {
             InitializeComponent();
+            BindingContext = this;
+        }
+
+
+    }
+    public class HyperlinkSpan : Span
+    {
+        public static readonly BindableProperty UrlProperty =
+            BindableProperty.Create(nameof(Url), typeof(string), typeof(HyperlinkSpan), null);
+
+        public string Url
+        {
+            get { return (string)GetValue(UrlProperty); }
+            set { SetValue(UrlProperty, value); }
+        }
+
+        public HyperlinkSpan()
+        {
+            TextDecorations = TextDecorations.Underline;
+            TextColor = Color.Blue;
+            GestureRecognizers.Add(new TapGestureRecognizer
+            {
+                // Launcher.OpenAsync is provided by Xamarin.Essentials.
+                Command = new Command(async () => await Launcher.OpenAsync(Url))
+            });
         }
     }
 }
